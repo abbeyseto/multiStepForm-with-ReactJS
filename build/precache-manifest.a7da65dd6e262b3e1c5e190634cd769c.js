2ef0d5e53c02c3dbb72fcb7036a31a5545e7b23f
Adenle Abiodun <adenleabbey@gmail.com>
2018-11-06 01:54:54 +0100

    Update precache-manifest.a7da65dd6e262b3e1c5e190634cd769c.js

--- build/precache-manifest.a7da65dd6e262b3e1c5e190634cd769c.js
+++ build/precache-manifest.a7da65dd6e262b3e1c5e190634cd769c.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "/build/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "022e82f89bd8071a2e10",
-    "url": "/static/js/main.022e82f8.chunk.js"
+    "url": "/build/static/js/main.022e82f8.chunk.js"
   },
   {
     "revision": "bcefb647ab5acc322d1b",
-    "url": "/static/js/1.bcefb647.chunk.js"
+    "url": "/build/static/js/1.bcefb647.chunk.js"
   },
   {
     "revision": "022e82f89bd8071a2e10",
-    "url": "/static/css/main.706ebbdd.chunk.css"
+    "url": "/build/static/css/main.706ebbdd.chunk.css"
   },
   {
     "revision": "393ebc7b496113ede6a3bd4dd7c42ade",
-    "url": "/index.html"
+    "url": "/build/index.html"
   }
 ];
